2516bb270320a2e4554ee517e321ec08e45e8392
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-11-14 04:41:54 +0900

    feat: reverse-proxy

---
+++ workers/reverse-proxy/src/index.ts
@@ -0,0 +1,49 @@
+addEventListener('fetch', (event) => {
+	event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request: Request) {
+	try {
+		const url = new URL(request.url);
+
+		if (url.pathname === '/') {
+			return new Response(`
+        Usage:\n
+          ${url.origin}/<url>
+      `);
+		}
+
+		if (request.method === 'OPTIONS') {
+			return new Response(null, {
+				status: 200,
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
+					'Access-Control-Allow-Headers': 'Authorization, Content-Type, Notion-Version',
+				},
+			});
+		}
+
+		const headers = new Headers(request.headers);
+
+		if (url.hostname === 'api.notion.com') {
+			const notionVersion = '2021-05-13';
+			headers.set('Notion-Version', notionVersion);
+		}
+
+		let response = await fetch(request.url.slice(url.origin.length + 1), {
+			method: request.method,
+			headers: headers,
+			redirect: 'follow',
+			body: request.body,
+		});
+		response = new Response(response.body, response);
+		response.headers.set('Access-Control-Allow-Origin', '*');
+		response.headers.set('Access-Control-Allow-Methods', 'GET, HEAD, POST, OPTIONS');
+		response.headers.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Authorization, Content-Type, Notion-Version');
+
+		return response;
+	} catch (e: any) {
+		return new Response(e.stack || e, { status: 500 });
+	}
+}
