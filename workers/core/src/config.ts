b70a4629ea637bda698c23ff86faa705958d64a0
yutakobaayshidev <hi@yutakobayashi.dev>
2024-03-19 03:44:42 +0900

    Add new variables to HonoConfig interface

--- workers/core/src/config.ts
+++ workers/core/src/config.ts
@@ -1,8 +1,21 @@
+import { Client } from "@notionhq/client";
+import { DiscordClient } from "./clients/discord";
+
+export type InternalContext = {
+  notion: Client;
+  discord: DiscordClient;
+};
+
+
 export interface HonoConfig {
   Bindings: {
     DISCORD_TOKEN: string;
+    OPENAI_API_KEY: string;
     DISCORD_APPLICATION_ID: string;
+    FORWARD_EMAIL: string;
+    CHANNEL_ID: string;
     DISCORD_PUBLIC_KEY: string;
+    NOTION_INBOX_DATABASE_ID: string;
     DISCORD_WEBHOOK_URL: string;
     DEEPL_API_KEY: string;
     SPOTIFY_CLIENT_ID: string;
@@ -10,6 +23,11 @@ export interface HonoConfig {
     SPOTIFY_REFRESH_TOKEN: string;
     NOTION_TOKEN: string;
     NOTION_DATABASE_ID: string;
+    YUTA_STUDIO: KVNamespace;
   };
-  Variables: {};
+  Variables: {
+    notion: Client;
+    discord: DiscordClient
+    internal: InternalContext;
+  };
 }
