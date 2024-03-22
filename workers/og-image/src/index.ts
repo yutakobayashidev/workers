5f9f989e4e10bddd7b70b42cdf7e06cdcfa31460
yutakobaayshidev <hi@yutakobayashi.dev>
2024-03-23 00:29:29 +0900

    Refactor OGPParser and fetch method

--- workers/og-image/src/index.ts
+++ workers/og-image/src/index.ts
@@ -1,64 +1,64 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+class OGPParser {
+	ogpTitle: string;
+	ogpDescription: string;
+	ogpImageUrl: string;
+	ogpSiteName: string;
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	constructor() {
+		this.ogpTitle = "";
+		this.ogpDescription = "";
+		this.ogpImageUrl = "";
+		this.ogpSiteName = "";
+	}
+	element(element: Element) {
+		switch (element.getAttribute("property")) {
+			case "og:title":
+				this.ogpTitle = element.getAttribute("content") ?? "";
+				break;
+			case "og:description":
+				this.ogpDescription = element.getAttribute("content") ?? "";
+				break;
+			case "og:image":
+				this.ogpImageUrl = element.getAttribute("content") ?? "";
+				break;
+			case "og:site_name":
+				this.ogpSiteName = element.getAttribute("content") ?? "";
+				break;
+			default:
+				break;
+		}
+	}
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+	async fetch(request: Request): Promise<Response> {
+
 		const url = new URL(request.url);
+
 		const targetUrl = url.searchParams.get('url');
 
 		if (!targetUrl) {
 			return new Response('URL parameter is missing', { status: 400 });
 		}
 
-		try {
-			const pageResponse = await fetch(targetUrl);
-			const pageHtml = await pageResponse.text();
-			const ogImageUrl = extractOgImage(pageHtml);
+		const decodedHref = decodeURIComponent(targetUrl);
+		const siteRes = await fetch(decodedHref);
 
-			if (!ogImageUrl) {
-				return new Response('OGP image not found', { status: 404 });
-			}
+		const ogp = new OGPParser();
+		const rewriter = new HTMLRewriter().on("meta", ogp);
+		await rewriter.transform(siteRes).text();
 
-			const imageResponse = await fetch(ogImageUrl);
-			const contentType = imageResponse.headers.get('Content-Type') || 'application/octet-stream';
-			return new Response(imageResponse.body, {
-				headers: {
-					'Content-Type': contentType,
-					'Cache-Control': 'max-age=604800',
-				},
-			});
-		} catch (error) {
-			return new Response('Error fetching OGP image', { status: 500 });
+		if (!ogp.ogpImageUrl) {
+			return new Response('OGP image not found', { status: 404 });
 		}
-	},
-};
 
-function extractOgImage(html: string): string | null {
-	const regex = /<meta property="og:image" content="(.*?)"/;
-	const match = regex.exec(html);
-	return match ? match[1] : null;
+		const imageResponse = await fetch(ogp.ogpImageUrl);
+		const contentType = imageResponse.headers.get('Content-Type') || 'application/octet-stream';
+		return new Response(imageResponse.body, {
+			headers: {
+				'Content-Type': contentType,
+				'Cache-Control': 'max-age=604800',
+			},
+		});
+	},
 }
