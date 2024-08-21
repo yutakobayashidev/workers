464e1130f316e37818be941056fb7444d83c9ba2
yutakobaayshidev <hi@yutakobayashi.dev>
2024-08-21 17:33:15 +0900

    chore: Remove unused dependencies and refactor email notification code

--- workers/core/src/email.ts
+++ workers/core/src/email.ts
@@ -1,9 +1,5 @@
 import { HonoConfig } from "./config";
 import PostalMime, { type Email } from 'postal-mime';
-import { loadSummarizationChain } from "langchain/chains";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { OpenAI } from "langchain/llms/openai";
-import { PromptTemplate } from "langchain/prompts";
 import { DiscordClient } from "./clients/discord";
 
 const email: ExportedHandler<HonoConfig["Bindings"]>["email"] = async (
@@ -15,19 +11,14 @@ const email: ExportedHandler<HonoConfig["Bindings"]>["email"] = async (
 };
 
 
-const buildNotifyMessage = async (message: ForwardableEmailMessage, parsedEmail: Email, env: HonoConfig["Bindings"]) => {
+const buildNotifyMessage = async (message: ForwardableEmailMessage, parsedEmail: Email) => {
 
-    const count = await env.YUTA_STUDIO.get(message.from) || "0";
-
-    await env.YUTA_STUDIO.put(message.from, (parseInt(count) + 1).toString(), { expirationTtl: 3600 });
-
-    const summary = parseInt(count) >= 5 ? "レート制限のため、要約をスキップしました" : await summarizeEmail(parsedEmail.text, env);
 
     return {
         embeds: [
             {
                 title: message.headers.get('subject') || "",
-                description: summary,
+                description: parsedEmail.text,
                 fields: [
                     {
                         name: "From",
@@ -56,35 +47,6 @@ const buildNotifyMessage = async (message: ForwardableEmailMessage, parsedEmail:
     };
 };
 
-async function summarizeEmail(text: string | undefined, env: HonoConfig["Bindings"]) {
-    const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
-    const docs = await textSplitter.createDocuments([text ?? ""]);
-
-    const prompt = new PromptTemplate({
-        inputVariables: ['text'],
-        template: `
-    Briefly summarize in Japanese the actions I should take based on the content of the following email received. Ignore the marketing email is an action:
-
-    "{text}"
-
-    CONCISE SUMMARY:
-    `
-    });
-
-    const summarizationChain = loadSummarizationChain(new OpenAI({ openAIApiKey: env.OPENAI_API_KEY, temperature: 0, modelName: "gpt-3.5-turbo" }), {
-        type: "map_reduce",
-        returnIntermediateSteps: true,
-        combineMapPrompt: prompt,
-        combinePrompt: prompt,
-    });
-
-    const summary = await summarizationChain.call({
-        input_documents: docs,
-    });
-
-    return summary.text;
-}
-
 const streamToArrayBuffer = async (stream: ReadableStream, streamSize: number) => {
     let result = new Uint8Array(streamSize);
     let bytesRead = 0;
@@ -109,7 +71,7 @@ const notifyMessage = async function (message: ForwardableEmailMessage, env: Hon
 
         const parsedEmail = await parser.parse(rawEmail);
 
-        const notifyMessage = await buildNotifyMessage(message, parsedEmail, env);
+        const notifyMessage = await buildNotifyMessage(message, parsedEmail);
 
         await message.forward(env.FORWARD_EMAIL);
 
